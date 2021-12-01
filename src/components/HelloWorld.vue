47c7e34aace0d20a4b857e94fd316ba33e226ead
dilshod1994 <mdilshod1994@gmail.com>
2021-12-02 00:27:00 +0500

    First commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="mob_nav">
+    <ul>
+      <li class="list active">
+        <router-link to="">
+          <span class="icon">
+           <ion-icon name="home-outline"></ion-icon>
+          </span>
+          <span class="text">
+            Home
+          </span>
+        </router-link>
+      </li>
+      <li class="list">
+        <router-link to="">
+          <span class="icon">
+            <ion-icon name="person-circle-outline"></ion-icon>
+          </span>
+          <span class="text">
+            Profile
+          </span>
+        </router-link>
+      </li>
+      <li class="list">
+        <router-link to="">
+          <span class="icon">
+            <ion-icon name="mail-open-outline"></ion-icon>
+          </span>
+          <span class="text">
+            Messages
+          </span>
+        </router-link>
+      </li>
+      <li class="list">
+        <router-link to="">
+          <span class="icon">
+            <ion-icon name="folder-open-outline"></ion-icon>
+          </span>
+          <span class="text">
+            About
+          </span>
+        </router-link>
+      </li>
+      <li class="list">
+        <router-link to="">
+          <span class="icon">
+            <ion-icon name="call-outline"></ion-icon>
+          </span>
+          <span class="text">
+            Contact
+          </span>
+        </router-link>
+      </li>
+      <div class="indicator"></div>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+
+    }
+  },
+  methods:{
+
+  },
+  mounted(){
+    const list = document.querySelectorAll('.list')
+    function activeLink() {
+      list.forEach((item)=>
+      item.classList.remove('active'))
+      this.classList.add('active')
+    }
+    list.forEach((item)=>
+      item.addEventListener('click', activeLink)
+    )
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
